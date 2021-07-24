697c0e9044b3cd21361b82c9ef26012ed3c1d17a
ganeles <33451225+ganeles@users.noreply.github.com>
2021-07-25 01:23:00 +0300

    Add files via upload

---
+++ consoleRun.bat
@@ -0,0 +1,1 @@
+c:\tools\jmeter\bin\jmeter.bat -n -t c:\Scenario\JmeterSmartScenario.jmx -Jbaseline_rampup=30 -Jbaseline_percent=100 -Jbaseline_duration=60 -Jstep_rampup=10 -Jstep_percent=20 -Jstep_duration=30
